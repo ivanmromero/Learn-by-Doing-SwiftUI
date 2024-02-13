79f4ed867532ff14d17520da443de9447c8fef06
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-13 14:33:26 -0300

    Initial config

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Ivan Romero on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
